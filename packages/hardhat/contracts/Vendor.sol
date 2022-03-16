a86aebad637422f614a23805888f8de6cce5d6dd
0xTranqui <thinkandchill11@gmail.com>
2022-03-16 19:45:28 -0300

    my implementation

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -6,18 +6,41 @@ import "./YourToken.sol";
 
 contract Vendor is Ownable {
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-
+  /// CONSTANTS
   YourToken public yourToken;
+  uint256 public amountOfEth;
+  uint256 public amountOfTokens;
+  uint256 public constant tokensPerEth = 100;
+
+  /// EVENTS
+  event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+  event SellTokens(address seller, uint256 amountOfETH, uint256 amountOfTokens);
+
+  /// MODIFIERS
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
 
   // ToDo: create a payable buyTokens() function:
+  function buyTokens() public payable {
+    amountOfEth = msg.value;
+    amountOfTokens = amountOfEth * tokensPerEth;
+    yourToken.transfer(msg.sender, amountOfTokens);
+    emit BuyTokens(msg.sender, amountOfEth, amountOfTokens);
+  }
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+  /// ToDo: create a withdraw() function that lets the owner withdraw ETH
+
+  function withdraw() external onlyOwner {
+    payable(msg.sender).transfer(address(this).balance);
+  }
 
   // ToDo: create a sellTokens() function:
+  function sellTokens(uint256 approvalAmount) public {
+    yourToken.transferFrom(msg.sender, address(this), approvalAmount);
+    payable(msg.sender).transfer(approvalAmount / tokensPerEth);
+    emit SellTokens(msg.sender, approvalAmount / tokensPerEth, approvalAmount);
+  }
 
 }
--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -5,7 +5,14 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract YourToken is ERC20 {
+
+    /// set initial supply to be minted by constructor
+    uint256 initialSupply = 1000 * (10 ** 18);
+
+    /// constructor deplies initial token contract and mints total supply
     constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+        _mint(msg.sender, initialSupply);
     }
+
+    
 }
